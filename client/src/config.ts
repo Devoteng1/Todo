1d593fc32c3f86b0322d9d039928a1efcd7932a4
Devoteng1 <lovebergers@gmail.com>
2022-08-31 15:49:27 +0000

    Added my project

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'm0y0iio325';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-rn-zm90l.eu.auth0.com',
+  clientId: 'QjlDbonKjo4kmrUrXBKCaQM9xsu1KH3F',
+  callbackUrl: 'http://localhost:3000/callback'
+};
